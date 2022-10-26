7908f263aee882e96ea0c10e4ed80fb6fa9a9fba
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-26 22:23:22 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BinaryTreeProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+           
+           
+        }
+    }
+}
